bdd42d0d5801693ef352409a45146f7b89f2ab61
Roman <Khlebnikov.Roman187@gmail.com>
2023-05-27 20:37:28 +0300

    Добавил вызов API через клиента

---
+++ WebApiClient/Program.cs
@@ -0,0 +1,14 @@
+﻿var text = File.ReadAllText("voina-i-mir.txt");
+var client = new WebApiClient.WordFrequencyClient("https://localhost:7215/", new HttpClient());
+var freq = await client.WordFrequencyAsync(text);
+PrintFrequencyToFile(freq, "out.txt");
+
+void PrintFrequencyToFile(IDictionary<string, int> frequency, string path)
+{
+    using var writer = new StreamWriter(path);
+
+    foreach (var i in frequency)
+    {
+        writer.WriteLine($"{i.Key} {i.Value}");
+    }
+}
